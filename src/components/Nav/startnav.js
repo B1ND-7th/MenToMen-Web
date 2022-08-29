02ed33616d84dc21d7dac2d85880b5afc00184ef
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-08-29 19:30:12 +0900

    Navbar

---
+++ src/components/Nav/startnav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./startnav.css";
+import logo from "../../img/Logo.png";
+
+export default function startNav() {
+    return (
+        <nav className='test'>
+            <div className='nav'>
+                <div className='navList'>
+                    <img className='logo' alt='' src={logo} />
+                    <h1>서비스 소개</h1>
+                </div>
+            </div>
+        </nav>
+    )
+}
