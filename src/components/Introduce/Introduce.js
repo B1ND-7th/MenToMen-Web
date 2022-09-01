3369c94e7d531f8e6f8ca3da08636579fe0ffafc
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-09-01 12:29:28 +0900

    FIX: 버튼 위치 수정

--- src/components/Introduce/Introduce.js
+++ src/components/Introduce/Introduce.js
@@ -6,10 +6,12 @@ import "./Introduce.css";
 import FirstSection from "./IntroduceSections/FirstSection";
 import SecondSection from "./IntroduceSections/SecondSection";
 import ThirdSection from "./IntroduceSections/ThirdSections";
+import StartNav from "../Nav/StartNav";
 function Introduce() {
   return (
     <>
       <div className="introduce">
+        <StartNav />
         <FirstSection />
         <SecondSection />
         <ThirdSection />
