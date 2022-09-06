fb4992393223838ae46aa87b3e9c540bafd19b25
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-09-06 19:42:16 +0900

    fix conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
-// import StartPage from "./components/StartPage/StartPage";
-// import StartPage from "./components/StartPage/StartPage"
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import AuthPage from "./page/AuthPage";
-// import AuthLoadingPage from "./page/AuthLoadingPage";
-// import StartNav from "./components/Nav/StartNav";
-// import File from "./components/File/File";
-// import Input from "./components/Input/Input";
+import StartPage from "./components/StartPage/StartPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthPage from "./page/AuthPage";
+import AuthLoadingPage from "./page/AuthLoadingPage";
+import StartNav from "./components/Nav/StartNav";
+import File from "./components/File/File";
+import Input from "./components/Input/Input";
 import Inputpage from "./page/Inputpage/Inputpage";
 import "./color.css";
 import LastButton from "./components/Button/LastButton/LastButton";
@@ -14,15 +13,15 @@ import LastButton from "./components/Button/LastButton/LastButton";
 function App() {
   return (
     <div>
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<StartPage />} />
           <Route path="/auth" element={<AuthPage />} />
           <Route path="/callback" element={<AuthLoadingPage />} />
         </Routes>
-      </BrowserRouter> */}
-      {/* <Inputpage /> */}
-      {/* <LastButton /> */}
+      </BrowserRouter>
+      <Inputpage />
+      <LastButton />
       <Inputpage />
 
     </div>
