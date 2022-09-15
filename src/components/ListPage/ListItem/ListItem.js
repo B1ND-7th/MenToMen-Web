5b37fa937caa887273b6673776ec55fe605f5dda
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-09-15 13:11:48 +0900

    Add 리스트 페이지 스타일

--- src/components/ListPage/ListItem/ListItem.js
+++ src/components/ListPage/ListItem/ListItem.js
@@ -2,14 +2,48 @@ import React from "react";
 import "./Listitem.css";
 
 const ListItem = ({ data }) => {
+  const date = new Date(data.localDateTime);
+  const week = ["일", "월", "화", "수", "목", "금", "토"];
+  const theHours = date.getHours();
+  const theMinutes = date.getMinutes();
+  console.log(theHours);
+
   return (
     <div className="formSection">
-      <div>{data.userName}</div>
-      <div>{data.tags}</div>
-      <div>{data.localDateTime}</div>
-      <div>{data.content}</div>
-      <div>
-        {data.stdInfo.grade}학년 {data.stdInfo.room}반 {data.stdInfo.number}번
+      <div className="leftSection">
+        <div className="profile">
+          <div className="profileUrl">
+            {data.profileUrl && (
+              <img
+                src={data.profileUrl}
+                className="profileUrl"
+                alt={"listItem profile"}
+              />
+            )}
+          </div>
+          <div className="nameTag">
+            <div className="userName">{data.userName}</div>
+            <div className="tagbox">
+              <div className="tag">{data.tag}</div>
+            </div>
+          </div>
+          <div className="date">
+            {`${date.getFullYear()}년 ${
+              date.getMonth() + 1
+            }월 ${date.getDate()}일 ${week[date.getDay()]}요일 `}
+            {theHours > 12
+              ? `오후 ${theHours - 12}시 ${theMinutes}분`
+              : `오전${theHours}`}
+          </div>
+        </div>
+        <div className="contentSection">{data.content}</div>
+        <div className="able">채팅이모티콘</div>
+      </div>
+
+      <div className="imgUrl">
+        {data.imgUrl && (
+          <img src={data.imgUrl} className="imgUrl" alt={"listItem img"} />
+        )}
       </div>
     </div>
   );
