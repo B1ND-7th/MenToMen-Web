fff35377447c305d831ee91373169b964b832075
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-10-06 21:37:08 +0900

    ⚡ ::  state상태 변경
    검색을 위해 전역으로 관리

--- src/components/ListPage/List.js
+++ src/components/ListPage/List.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import { customAxios } from "../../lib/axios/customAxios.js";
 import ListItem from "./ListItem/ListItem.js";
 import "./List.css";
-
+import Search from "../Search/Search.js";
+import { useRecoilState } from "recoil";
+import { listState } from "../../recoil/listAtom";
 function List() {
-  const [list, setList] = useState([]);
+  const [list, setList] = useRecoilState(listState);
 
   const request = async () => {
     try {
       const { data } = await customAxios.get("/post/read-all");
+      setList(data);
       console.log(data);
       setList(data.data);
     } catch (error) {}
@@ -20,6 +23,7 @@ function List() {
 
   return (
     <>
+      <Search />
       <div className="listSection">
         {list.map((item, idx) => (
           <ListItem data={item} key={item.name + " " + idx} />
---
+++ src/recoil/listAtom.js
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const listState = atom({
+  key: "feedImg",
+  default: [],
+});
