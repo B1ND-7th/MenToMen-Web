ce81b4643eca96aca75f6d70c416a700be9e0ccc
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-10-17 13:14:36 +0900

    수정

--- src/api/auth/Comment.api.js
+++ src/api/auth/Comment.api.js
@@ -1,7 +1,7 @@
 import { customAxios } from "../../lib/axios/customAxios";
 
 const commentApi = async (postId) => {
-  const { data } = await customAxios.get(`api/comment/read/${postId}`);
+  const { data } = await customAxios.get(`/comment/read/${postId}`);
   return data;
 };
 
